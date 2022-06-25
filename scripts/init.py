fba834a1168419d2d2d997af8c24f05327d29c74
Gakawarstone <pinnekerv@gmail.com>
2022-06-25 21:40:49 +0600

    chore: update init script

--- scripts/init.py
+++ scripts/init.py
@@ -7,4 +7,11 @@ if not os.path.isfile('.env'):
         print('Paste following data')
         token = input('Telegram bot token: ')
         file.write('BOT_TOKEN = ' + token + '\n')
+        backend_url = input('Back-end URL: ')
+        file.write('BACKEND_URL = ' + backend_url + '\n')
+        private_key_path = input('Private key path: ')
+        file.write('BOT_PRIVATE_KEY_PATH = ' + private_key_path + '\n')
+        public_key_path = input('Public key path: ')
+        file.write('BACKEND_PUBLIC_KEY_PATH = ' + public_key_path + '\n')
+
     print('You can manually change it in .env file')
