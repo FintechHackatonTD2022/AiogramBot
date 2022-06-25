ad2de842167a932a4c2e7bfe807ee18e8452cf2a
alphatoaster <onixcontactmail@gmail.com>
2022-06-25 19:37:43 +0600

    chore: update bot_config.py

--- src/bot_config.py
+++ src/bot_config.py
@@ -18,10 +18,14 @@ load_dotenv()
 # VARS
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 BACKEND_URL = os.getenv('BACKEND_URL')
+BOT_PRIVATE_KEY_PATH = os.getenv('BOT_PRIVATE_KEY_PATH')
+BACKEND_PUBLIC_KEY_PATH = os.getenv('BACKEND_PUBLIC_KEY_PATH')
 
 # Log info vars
 logger.info('BOT_TOKEN = ' + BOT_TOKEN)
 logger.info('BACKEND_URL = ' + BACKEND_URL)
+logger.info('BOT_PRIVATE_KEY_PATH =' + BOT_PRIVATE_KEY_PATH)
+logger.info('BOT_PRIVATE_KEY_PATH =' + BACKEND_PUBLIC_KEY_PATH)
 
 # main objects
 bot = Bot(BOT_TOKEN)
